96d42a52472a87459e612d1b5ac1ebf9fae49e9a
dmaharaj <david.maharaj@gmail.com>
2020-01-17 14:25:16 -0800

    Updates

---
+++ precache-manifest.f5f068893f11e21e3d3b37e7ab110adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a928adbcaeb6846d1b079a6ac7fb834d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbef84402bd1bb339458",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5b129404f865e8ebd16e",
+    "url": "/static/js/2.ef33109f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ef33109f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbef84402bd1bb339458",
+    "url": "/static/js/main.1bd14541.chunk.js"
+  },
+  {
+    "revision": "fd2e3770b85456741325",
+    "url": "/static/js/runtime-main.2741536f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
